6f713d03f93c7f629fe942ae84699cc369bf90af
Deepak <deepakpatidar.thoughtwin@gmail.com>
2021-08-26 19:19:11 +0530

    exeception handling

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import requests
+from great_text import great_text
 
 _QUOTE_URL = "https://quotes.rest/qod"
 
@@ -7,13 +8,18 @@ def get_day_quote():
     """Get a day quote."""
 
     res = requests.get(_QUOTE_URL)
-    return res.json()["contents"]["quotes"][0]["quote"]
+    try:
+        q = res.json()["contents"]["quotes"][0]["quote"]
+    except KeyError:
+        err = "Internet Connection Error >>>"
+        q = great_text(err, "red")
+    return q
 
 
 def display_quote():
     """Display a random quote."""
-
-    print(f'Quote Of The Day: "{get_day_quote()}"')
+    quote = f'Quote Of The Day: "{get_day_quote()}"'
+    great_text(quote, "red")
 
 
 if __name__ == "__main__":
